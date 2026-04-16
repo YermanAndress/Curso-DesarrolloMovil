6628a2cc2819e4a9ca750256bb58aeec1ea31ca3
YermanAndress <geranroca1120@gmail.com>
2026-04-16 08:34:17 -0500

    Feature: Añadir View Ciclo De Vida, Future E Isolate

---
+++ taller2/lib/routes/app_router.dart
@@ -0,0 +1,51 @@
+import 'package:go_router/go_router.dart';
+import 'package:actividad_clase/views/ciclo_vida/ciclo_vida_screen.dart';
+import 'package:actividad_clase/views/home/home_screen.dart';
+import 'package:actividad_clase/views/paso_parametros/detalle_screen.dart';
+import 'package:actividad_clase/views/paso_parametros/paso_parametros_screen.dart';
+import 'package:actividad_clase/views/future/future_view.dart';
+import 'package:actividad_clase/views/isolate/isolate_view.dart';
+
+final GoRouter appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(), // Usa HomeView
+    ),
+    // Rutas para el paso de parámetros
+    GoRoute(
+      path: '/paso_parametros',
+      builder: (context, state) => const PasoParametrosScreen(),
+    ),
+
+    // !Ruta para el detalle con parámetros
+    GoRoute(
+      path:
+          '/detalle/:parametro/:metodo', //la ruta recibe dos parametros los " : " indican que son parametros
+      builder: (context, state) {
+        //*se capturan los parametros recibidos
+        // declarando las variables parametro y metodo
+        // es final porque no se van a modificar
+        final parametro = state.pathParameters['parametro']!;
+        final metodo = state.pathParameters['metodo']!;
+        return DetalleScreen(parametro: parametro, metodoNavegacion: metodo);
+      },
+    ),
+    //!Ruta para el ciclo de vida
+    GoRoute(
+      path: '/ciclo_vida',
+      name: 'ciclo_vida',
+      builder: (context, state) => const CicloVidaScreen(),
+    ),
+    GoRoute(
+      path: '/isolate',
+      name: 'isolate',
+      builder: (context, state) => const IsolateView(),
+    ),
+    GoRoute(
+      path: '/future',
+      name: 'future',
+      builder: (context, state) => const FutureView(),
+    ),
+  ],
+);
